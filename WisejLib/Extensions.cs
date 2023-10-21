bacd5f546659451fc64d87f5f52e36b2cfd07920
Joe Meyer @ Home <joachim.meyer@talkserv.de>
2023-10-21 09:53:20 +0200

    removed duplicate RemoveDiacritics

--- WisejLib/Extensions.cs
+++ WisejLib/Extensions.cs
@@ -174,23 +174,26 @@ namespace WisejLib
             return list.ListAsString<int>(buildFunction, separator);
         }
 
-        public static string RemoveDiacritics(this string text)
-        {
-            var normalizedString = text.Normalize(NormalizationForm.FormD);
-            var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+        // use System.StringExtensions.RemoveDiacritics
 
-            for (int i = 0; i < normalizedString.Length; i++)
-            {
-                char c = normalizedString[i];
-                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-                {
-                    stringBuilder.Append(c);
-                }
-            }
+        //public static string RemoveDiacritics(this string text)
+        //{
+        //    System.StringExtensions.RemoveDiacritics
+        //    var normalizedString = text.Normalize(NormalizationForm.FormD);
+        //    var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
 
-            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
-        }
+        //    for (int i = 0; i < normalizedString.Length; i++)
+        //    {
+        //        char c = normalizedString[i];
+        //        var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+        //        if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+        //        {
+        //            stringBuilder.Append(c);
+        //        }
+        //    }
+
+        //    return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        //}
 
         public static string Right(this string value, int length)
         {
